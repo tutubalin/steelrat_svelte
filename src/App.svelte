40de6b853a44813668098596ff0d527a4e61ccbd
Daniil Tutubalin <tutubalin@gmail.com>
2020-01-05 02:57:52 +0300

    Now saves progress

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,21 @@
 	import Chapter from './Chapter.svelte'
 	import chapters from './chapters.js'
 
-	let current_chapter = 0;
+	let story;
+
+	const json = localStorage.getItem("story");
+	if (json) {
+		story = JSON.parse(json);
+	} else {
+		story = [0];
+	}
+
+	let current_chapter = story[story.length-1];
 
 	function handleMove(event) {
 		current_chapter = event.detail;
+		story.push(+current_chapter);
+		localStorage.setItem("story", JSON.stringify(story));
 	}
 </script>
 
